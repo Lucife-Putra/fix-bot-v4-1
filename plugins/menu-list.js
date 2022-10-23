904951551e8cb99f3b7073c20f79db12b1388de4
Lucife-Putra <113871330+Lucife-Putra@users.noreply.github.com>
2022-10-24 03:26:46 +0700

    Update menu-list.js

--- plugins/menu-list.js
+++ plugins/menu-list.js
@@ -360,7 +360,7 @@ let tek = `✧────···[ Dashboard ]···────✧
 │
 ├━━━━━━━━━━━━━━━━┈─⋆
 │
-│ ▸ *Sumber :* YouTube WH-MODS-DEV
+│ ▸ *Sumber :* Chat owner
 │ ▸ *ᴀᴜᴛʜᴏʀ :* ${nameown}
 ┴ ▸ *ᴏᴡɴᴇʀ :* ${nameown}
 ✧
