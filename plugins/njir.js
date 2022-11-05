525fb1857430032161b795e56d4ff77cc23ca75e
PUTRAMODZ <113871330+Lucife-Putra@users.noreply.github.com>
2022-11-05 15:24:27 +0700

    Add files via upload

--- plugins/njir.js
+++ plugins/njir.js
@@ -1,8 +1,8 @@
 import fs from'fs'
 let { MessageType } = (await import('@adiwajshing/baileys')).default
 let handler = async (m, { conn }) => {
-let njirrr = fs.readFileSync('./mp3/lu siapa anjir.mp3') 
-conn.sendFile(m.chat, njirr, '', '', m, true)
+let apah = fs.readFileSync('./mp3/lu siapa anjir.mp3') 
+conn.sendFile(m.chat, apah, '', '', m, true)
 }
 
 handler.customPrefix = /^(njirr|anjir|njir|anjirr)$/i
